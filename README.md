30df5e92993115bdee5066eed8360fa579b863f4
Oliver Beckstein <orbeckst@gmail.com>
2021-01-31 03:16:04 -0700

    enhanced generate_tests.py
    - can use custom tests
    - build tests into "BUILD"/<name>
    - copies test assets as needed (eg tst.py for templated tests)
    - updated README

--- README.md
+++ README.md
@@ -1,40 +1,75 @@
 # Homeworks Test Templates
 
-Homework tests are generated from templates and a `solution.yml` file that contains the individual tests with points. The `generate_tests.py` script then creates
+Homework tests are generated from templates and a `solution.yml` file
+that contains the individual tests with points. The
+`generate_tests.py` script then creates
 
 - the appropriate tests themselves
 - the `autograding.json` configuration for the `.github/workflows/classroom/autograding.json`
 
+The `solutions.yml` file indicates problems with their individual
+tests. For each problem, a subdirectory is made under tests. Each test
+gets its own test file, point value, and entry in `autograding.yml`.
+
+At the moment the following is supported:
+- template tests for checking fixed variables
+- template tests for running a script with `input()` from stdin that sets variables
+- custom test files
+
 
 ## Usage
 
-Primitive at the moment...
+Primitive at the moment... will create the new HW in a directory
+`BUILD/HWxx`, which needs to be manually copied as necessary.
+
+
 
 ### Assignment
-Create the assignment.md and symlink to README.md.
+Create the assignment.md.
 
 Make sure to include the points banner.
+
+### Starter code
+
+Manually copy any starter code.
 
 ### Classroom workflow
 1. Manually copy a working `.github/workflows/classroom.yml`
 
 
-### Tests infrastructure
-1. In hw working dir, `mkdir tests` 
-2. Manually copy `tst.py` to `tests` directory.
-
-
 ### Generating tests
 
-1. cd hw working directory tests dir
-2. symlink `solution.yml` to the hw tests working directory
-3. run `generate_tests.py` in the hw tests working dir 
-4. mv new autograding.json to `.github/classroom`
-5. add & commit all new files
+Tests are generated in the BUILD directory, typically "BUILD/HWxx"; we
+will refer to it as `$BUILD`.
+
+1. cd hw working directory with `solution.yml` 
+2. run `generate_tests.py`, which creates `$BUILD`
+
+
+### Tests infrastructure
+
+All necessary files are copied into the `$BUILD` directory. 
+
+### Final assembly
+
+- copy assignment.md to `$BUILD` and symlink as README.md
+- copy assignment assets to `$BUILD`
+- `mkdir $BUILD/.github/{workflows,classroom}`
+- copy classroom.json `cp $ASSETS/workflows/classroom.yml $BUILD/.github/workflows` (does not change)
+- copy autograding.json `cp autograding.json $BUILD/.github/classroom` (is specific for this assignment)
+- create .gitignore
+- create a  *template* repository for the HWxx,  `cd $BUILD && git init && git add . && git commit`, push to the repo
+
+
 
 
 ## Solutions
-Keep solutions in a `solution` directory here for reference.
+
+Keep solutions in a `solution` directory here for reference. They are
+not used anywhere. The `solution.yml` file needs to either contain the
+correct reference values or the custom tests need to contain the code
+to check for correctness.
+
 
 
 ## TODO
--- bin/generate_tests.py
+++ bin/generate_tests.py
@@ -6,6 +6,7 @@
 import string
 import pathlib
 import os.path
+import shutil
 import yaml
 import json
 
@@ -32,7 +33,8 @@ import json
 
 ASSETS = pathlib.Path(__file__).parent / os.path.pardir / "assets"
 
-templatedir = ASSETS / "testing" / "templates"
+testing_assets_dir = ASSETS / "testing"
+templatedir = testing_assets_dir / "templates"
 templates = {'variable':
              string.Template((templatedir / '_test_variable.template.py').read_text()),
              'multi_variables':
@@ -40,6 +42,8 @@ templates = {'variable':
              'variable_with_input':
              string.Template((templatedir / '_test_variable_with_input.template.py').read_text()),
              }
+
+template_dependencies = [testing_assets_dir / "tst.py"]
 
 autograder = {
     "name": None,     # replace
@@ -55,8 +59,13 @@ autograder = {
 
 
 def choose_template(problem):
+    if 'test' in problem:
+        # custom test under ./tests (from where solution.yml sits)
+        return "tests" / pathlib.Path(problem['test'])
+
     if 'variable' not in problem:
         raise ValueError("Only templates with variable checks implemented")
+
     if 'input_values' in problem:
         return templates['variable_with_input']
     if isinstance(problem['variable'], list):
@@ -65,8 +74,16 @@ def choose_template(problem):
     # simple: just one variable
     return templates['variable']
 
-# def stringify_dict(d):
-#     return {k:str(v) for k,v in d.items()}
+def make_safe_filename(filename, keepcharacters=('.', '-', '_')):
+    # based on https://stackoverflow.com/a/7406369
+    return "".join(c for c in filename if c.isalnum() or c in keepcharacters).strip()
+
+def create_init_file(directory, comment):
+    initfile = directory / pathlib.Path("__init__.py")
+    with initfile.open("w") as out:
+        out.write(f"# {comment}\n")
+    print(f"+ Created {initfile}")
+    return initfile
 
 if __name__ == "__main__":
 
@@ -78,7 +95,29 @@ if __name__ == "__main__":
     tests_list = []  # becomes "tests": test_list in autograding.json
     points_total = 0
 
+    build_dir = pathlib.Path("BUILD") / make_safe_filename(problemset['name'])
+    test_dir = build_dir / "tests"
+
     print(f"assignment: {problemset['name']}")
+    print(f"BUILD_DIR: {build_dir}")
+    print(f"test_dir: {test_dir}")
+
+    build_dir.mkdir(parents=True, exist_ok=True)
+    test_dir.mkdir(parents=True, exist_ok=True)
+
+    # custom global assets
+    for filename in problemset.get('test_assets', []):
+        # find test assets under tests/
+        asset_path = "tests" / pathlib.Path(filename)
+        shutil.copy(asset_path, test_dir)
+        print(f"+ Copied {asset_path} --> {test_dir}")
+
+    # make it a package for relative imports
+    create_init_file(test_dir, f"tests for {problemset['name']}")
+
+    # If we use any of the templates then we also need to copy other
+    # assets.
+    used_templates = False
 
     for problem in problemset['problems']:
         print(f"= problem: {problem['problem']} in '{problem['filename']}'")
@@ -86,16 +125,13 @@ if __name__ == "__main__":
                     'problem': problem['problem'],
                     'filename': problem['filename'],
                     }
-        problem_dir = pathlib.Path(f"problem_{problem['problem']}")
+        problem_dir = test_dir / pathlib.Path(f"problem_{problem['problem']}")
         problem_dir.mkdir(exist_ok=True)
         print(f"+ Created {problem_dir}/")
 
         # make it a package for relative imports
-        initfile = problem_dir / "__init__.py"
-        with open(initfile, "w") as out:
-            out.write(f"# tests for {problemset['name']}: Problem {problem['problem']}\n")
-
-        print(f"+ Created {initfile}")
+        create_init_file(problem_dir,
+                         f"tests for {problemset['name']}: Problem {problem['problem']}")
 
 
         for subproblem in problem['items']:
@@ -104,12 +140,19 @@ if __name__ == "__main__":
             subs.setdefault('check_type', False)
             subs.update(metadata)
             template = choose_template(subs)
-            t = template.substitute(subs)
-            #print(t)
 
             testfile = problem_dir / f"test_{subproblem['name']}.py"
-            with open(testfile, 'w') as out:
-                out.write(t)
+            if isinstance(template, string.Template):
+                print(f".. Using standard template ")
+                used_templates = True
+                t = template.substitute(subs)
+                #print(t)
+                with open(testfile, 'w') as out:
+                    out.write(t)
+            else:
+                # custom file: just copy
+                print(f".. Copy custom test {template}")
+                shutil.copy(template, testfile)
 
             print(f"++ Created {testfile}... [{subproblem['points']}]")
 
@@ -124,8 +167,14 @@ if __name__ == "__main__":
 
             points_total += subproblem['points']
 
+    # copy standard test assets if needed
+    if used_templates:
+        for asset_path in template_dependencies:
+            shutil.copy(asset_path, test_dir)
+            print(f"+ Copied {asset_path} --> {test_dir} (dependency)")
+
     # write autograder json
-    ag_json = pathlib.Path("autograding.json")
+    ag_json = build_dir / pathlib.Path("autograding.json")
     ag_json.write_text(json.dumps({'tests': tests_list}))
 
     print(f"+ Created '{ag_json}'... [TOTAL {points_total}]")
