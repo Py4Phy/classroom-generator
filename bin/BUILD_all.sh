a92d0dcc7c2a70de2c3663783175f276f08b6619
Oliver Beckstein <orbeckst@gmail.com>
2021-02-02 11:37:20 -0700

    quick and dirty build-all script

---
+++ bin/BUILD_all.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+BUILD=$1
+TOPDIR=/Volumes/ASU/oliver/Documents/Teaching/ASU/CompPhys_PHY494/2021/Classroom/classroom-generator
+ASSETS=$TOPDIR/assets
+BINDIR=$TOPDIR/bin
+
+
+function die () {
+    echo "ERROR: $1"
+    exit 1;
+}
+
+$BINDIR/generate_tests.py || die "generator failed"
+
+test -n "$BUILD" || die "usage: $0 BUILD_DIR"
+test -d "$BUILD" || die "no dir $BUILD"
+
+echo "## Finalizing repo"
+
+cp -v README.md $BUILD
+cp -v *.py $BUILD
+test -d tests && rsync -avP --exclude="__pycache__" --exclude="*~" tests $BUILD
+mkdir -p $BUILD/.github/{workflows,classroom}
+cp -v $ASSETS/workflows/classroom.yml $BUILD/.github/workflows
+mv $BUILD/autograding.json $BUILD/.github/classroom/
+cp -v $ASSETS/students.gitignore $BUILD
+
+(cd $BUILD && test -d .git ||  { git init && git add -A . && git commit -m "initialized assignment"; } && { git add -A . && git commit -m "updated assignment"; })
