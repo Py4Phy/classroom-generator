138b9c69095d2d1db217807127e0769b75ec054d
Oliver Beckstein <orbeckst@gmail.com>
2021-02-18 04:35:00 -0700

    BUILDER: include any tex/*.pdf

--- bin/BUILD_all.sh
+++ bin/BUILD_all.sh
@@ -18,7 +18,7 @@ test -d "$BUILD" || die "no dir $BUILD"
 
 echo "## Finalizing repo"
 
-cp -v README.md assignment*.md assignment*.pdf tex/assignment*.pdf $BUILD
+cp -v README.md assignment*.md assignment*.pdf tex/*.pdf $BUILD
 cp -v *.csv *.txt $BUILD
 cp -v *.py $BUILD
 # Should not need to copy tests: use test_assets: ["conftest.py", ...] at top.
